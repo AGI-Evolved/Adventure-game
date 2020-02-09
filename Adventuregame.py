de410ebae88db01e8c1d67f38c581aa6ff2d4569
AGI-Evolved <33582003+AGI-Evolved@users.noreply.github.com>
2020-02-09 19:47:34 +0000

    Add files via upload

---
+++ Adventuregame.py
@@ -0,0 +1,55 @@
+
+answer = input("Wanna play a game? (yes/no) ")
+
+if answer.lower().strip() == "yes":
+
+    answer = input("You reach a crossroad, would you like to go left or right?").lower().strip()
+    if answer == "left":
+        answer = input("You encounter a monster, would you like to run or attack?")
+
+        if answer == "attack":
+            print("That was not the best idea, you are now monster chow! Game over.")
+        else:
+            print("Good choice, you got away safely.")
+
+            answer = input("You see a boat and a cart, which would you like to get in?")
+
+            if answer == "boat":
+                print("You're eaten by a whale mother fucker, welcome to the high seas bitch! Game over.")
+
+            elif answer == "cart":
+                print("Good choice, you stow away in the back of a giants cart, hidden behind giant sex toys. When the giant returns she takes the cart over a cross bridge and into the courtyard of the king of the giants castle. The giant leaves the cart and starts twerking with the other giants. This is your chance.")
+
+                answer = input("Do you run or hide?")
+
+                if answer == "hide":
+                    print("The giant returns for her barrel sized butt plug and finds you hiding behind it. She loos you up and down, then bites your head off. Game over.")
+                
+                elif answer == "run":
+                    print("You run into the castle and find a hen shitting out gold eggs... Jackpot! You grab the hen and see the exit just ahead, however there is another room deeper into the castle from which you see the glimmer of gold.")
+
+                    answer = input("Do you escape or investigate?")
+
+                    if answer == "escape":
+                        print("You sneak past the twerking giants with your gold shitting hen and run all the way home to your village. You soon become the wealthiest person you know, and rouse jealousy amongst your neigbours. Seven seasons pass, then on a cold winters evening you are lying on your bed of gold thinking of how great you are, when Olef, the village idiot sneaks into your room and slits your throat. Game over.")
+
+                    elif answer == "investigate":
+                        print("You sneak into this glimmering room. You struggle to beleive your eyes, ahead of you are mountains of golden eggs. Sat cradled next to one of these mountains you see the king of the giants, it takes you a moment to process what you are seeing. He has tears of joy in his eyes and is stark bollock naked. You realise that he is continuously grabbing a handfull of golden eggs and inserting them inside himself, like some form of shiny suppository. He spots you and smiles kindly before asking if you would like do join him.")
+
+                        answer = input("Do you say yes or no?")
+
+                        if answer == "no":
+                            print("The giant king is offended and picks you up with his next handful of golden eggs. You spend your last living moments up a giant kings bum hole. Game over.")
+
+                        elif answer == "yes":
+                            print("You nuzzle up next to the giant king, and help him insert as many golden eggs up his bum as possible. Becoming his bitch for all eternity. Congratulations you win!")
+
+
+    elif answer == "right":
+        print("You fall into a monsters anus and drown, unlucky bitch tits! Game over.")
+
+    else:
+        print("invalid choice, Game over.")
+
+else:
+    print("That's too bad, bitch tits!")
